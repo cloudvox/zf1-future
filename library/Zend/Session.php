789bc23db14b89bd14ba12535d9e56bec88b9b98
frederic <frederic.zapataalma@gmail.com>
2020-09-09 09:38:14 -0700

    DTPORTAL-19264 fixing bad merge conflict resolution

--- library/Zend/Session.php
+++ library/Zend/Session.php
@@ -546,7 +546,7 @@ class Zend_Session extends Zend_Session_Abstract
             $hashBitsPerChar = 5; // the default value
         }
         $pattern = '';
-        switch ($bitsPerChar) {
+        switch ($hashBitsPerChar) {
             case 4: $pattern = '^[0-9a-f]*$'; break;
             case 5: $pattern = '^[0-9a-v]*$'; break;
             case 6: $pattern = '^[0-9a-zA-Z-,]*$'; break;
